8855323b7092652362149fdb66b130cf594cedf7
EzequielVyG <evalleygarcia@gmail.com>
2025-04-07 19:28:26 -0300

    footer

---
+++ frontend/src/app/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../components/Footer"; 
+
+const App: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+        <h1 className="text-2xl text-center mt-10">Bienvenido a Madryn Buceo</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
