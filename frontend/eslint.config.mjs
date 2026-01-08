5ee5de1ed263017f87a590d95659599cb2402dc3
Maximo Ozonas <90213060+maxiozonas@users.noreply.github.com>
2026-01-08 14:04:45 -0300

    Add ESLint rules for .cjs files

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+  {
+    files: ["**/*.cjs"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }]
+    },
+  },
 ];
 
 export default eslintConfig;
